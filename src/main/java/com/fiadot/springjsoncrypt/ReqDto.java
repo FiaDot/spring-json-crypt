1240f5190694e08963dc5292179605d441930f2f
fiadot <fiadot@gmail.com>
2014-02-06 15:36:45 +0900

    JSON communication (jackson)

---
+++ src/main/java/com/fiadot/springjsoncrypt/ReqDto.java
@@ -0,0 +1,16 @@
+package com.fiadot.springjsoncrypt;
+
+
+
+public class ReqDto {
+	private String plain_data;
+
+	public String getPlain_data() {
+		return plain_data;
+	}
+
+	public void setPlain_data(String plain_data) {
+		this.plain_data = plain_data;
+	}
+	
+}
---
+++ src/main/java/com/fiadot/springjsoncrypt/ResDto.java
@@ -0,0 +1,16 @@
+package com.fiadot.springjsoncrypt;
+
+
+
+public class ResDto {	
+	private String encrypted_data;
+
+	public String getEncrypted_data() {
+		return encrypted_data;
+	}
+
+	public void setEncrypted_data(String encrypted_data) {
+		this.encrypted_data = encrypted_data;
+	}
+	
+}
